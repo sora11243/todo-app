478acd8e0f04a2ab802d943ac4753441edb0fb02
302A-08\cre <a>
2024-10-15 16:08:12 +0900

    todo-app

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
-use Illuminate\Support\Facades\Validator;//追加
+use Illuminate\Support\Facades\Validator; //追加
 
 class TaskController extends Controller
 {
@@ -13,7 +13,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('tasks.index');
+        $tasks = Task::where('status', false)->get();
+
+        return view('tasks.index', compact('tasks'));
     }
 
     /**
@@ -28,24 +30,26 @@ class TaskController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {$rules = [
-        'task_name' => 'required|max:100',
-      ];
-     
-      $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
-     
-      Validator::make($request->all(), $rules, $messages)->validate();
+    {
+        $rules = [
+            'task_name' => 'required|max:100',
+        ];
 
-         //モデルをインスタンス化
-        $task=new task;
-        //モデル->カラム名 = 値で、データを割り当てる
-        $task_name = $request->input('task_name');
+        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+        Validator::make($request->all(), $rules, $messages)->validate();
+        //モデルをインスタンス化
+        $task = new Task;
+
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $task->name = $request->input('task_name');
+
         //データベースに保存
         $task->save();
+
         //リダイレクト
         return redirect('/tasks');
     }
-
     /**
      * Display the specified resource.
      */
@@ -59,7 +63,8 @@ class TaskController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $task = Task::find($id);
+        return view('tasks.edit', compact('task'));
     }
 
     /**
@@ -67,7 +72,41 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+            $rules = [
+                'task_name' => 'required|max:100',
+            ];
+
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+            Validator::make($request->all(), $rules, $messages)->validate();
+
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
+
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+
+            //データベースに保存
+            $task->save();
+        }
+
+
+        //リダイレクト
+        return redirect('/tasks');
     }
 
     /**
@@ -75,6 +114,8 @@ class TaskController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        Task::find($id)->delete();
+
+        return redirect('/tasks');
     }
 }
